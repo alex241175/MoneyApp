bb45e05bfddbf4fe5e63b70964cfe99528175a52
alex241175 <alex2411@gmail.com>
2022-02-05 23:01:41 +0800

    202202052301

--- Data/Account.cs
+++ Data/Account.cs
@@ -6,7 +6,7 @@ namespace MoneyApp.Data
         public string? Name { get; set; }
         public string? Currency { get; set; }
         public int UserId { get; set; } 
-        public float? ToBaseExchangeRate {get;set;}  //Base is MYR
+        public float ToBaseExchangeRate {get;set;}  //Base is MYR
         public virtual List<Transaction> Transactions {get;set;}   
 
     }
